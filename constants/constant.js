5409f3bbb129c6568c6b06740dc50792afbea318
NiteshPant <nitesh.pant@xlscout.com>
2024-05-27 23:49:59 +0530

    updated url
    updated url port

--- constants/constant.js
+++ constants/constant.js
@@ -7,7 +7,7 @@ const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
 const MONGO_PASS = process.env.MONGO_PASS
 const MONGO_USERNAME = process.env.MONGO_USERNAME
 
-const SHORT_URL_LINK = "http://43.205.62.246:3000/api/get-file/"
+const SHORT_URL_LINK = "http://43.205.62.246:8000/api/get-file/"
 module.exports = {
     AWS_ACCESS_KEY,
     AWS_SECRET_ACCESS_KEY,
